976cd6d5b77674021a629ad29cf2cfde416caff5
pymachado <pyuris22@gmail.com>
2021-12-17 23:22:08 -0500

    added BubbleSort

--- BubbleSort.sol
+++ BubbleSort.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.0;
+/**
+    @author Pedro Machado
+    @notice Bubble sort, sometimes referred to as sinking sort, 
+    is a simple sorting algorithm that repeatedly steps through the list, 
+    compares adjacent elements and swaps them if they are in the wrong order. 
+    The pass through the list is repeated until the list is sorted.
+ */
 
 library BubbleSort {
 
