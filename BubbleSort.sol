94df477e9bdd3c5f3e93a1b9e9fa25483ffa1da9
pymachado <pyuris22@gmail.com>
2021-12-18 01:04:57 -0500

    added BubbleSort

--- BubbleSort.sol
+++ BubbleSort.sol
@@ -12,18 +12,19 @@ pragma solidity ^0.8.0;
 library BubbleSort {
 
     function bubbleSort(uint[] memory array) internal pure returns(uint[] memory){
-        require(array.length != 0, "BurbleSort: Array is null");
-        for(uint i = 0; i <= array.length - 1; i++) {
-            for(uint j = i+1; j <= array.length - 1; j++) {
-                if(array[i] > array[j]) {
-                    uint val = array[i];
-                    array[i] = array[j];
-                    array[j] = val;
+        require(array.length != 0, "BubbleSort: Array is empty");
+        uint count = array.length;
+        while (count > 0) {
+            for (uint i = 1; i <= array.length - 1; i++) {
+                if (array[i] < array[i-1]) {
+                    uint val = array[i-1];
+                    array[i-1] = array[i];
+                    array[i] = val;
                 }
             }
+            count --;
         }
-        return array;
-    }
+        return array;        
 }
 
 contract Example {
